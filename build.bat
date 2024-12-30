4f876c8a0d4ce5114da4ac9f0bbb3c0e1f152bdc
Bradley Saunders <bradley@vrchat.com>
2024-12-30 18:21:30 -0500

    Use 'powershell' instead of pwsh
    I guess on windows 11 even after wingeting powershell it doesn't setup the pwsh environment varaible. 'powershell' should exist though

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 del /f "VOICEVOX Audio Generator.ankiaddon"
-pwsh -Command "& {Compress-Archive -LiteralPath __init__.py, ffmpeg.py, voicevox_gen.py, config.json, config.md, manifest.json, README.md -DestinationPath VOICEVOX-Audio-Generator.zip -Force}"
+powershell -Command "& {Compress-Archive -LiteralPath __init__.py, ffmpeg.py, voicevox_gen.py, config.json, config.md, manifest.json, README.md -DestinationPath VOICEVOX-Audio-Generator.zip -Force}"
 rename VOICEVOX-Audio-Generator.zip "VOICEVOX Audio Generator.ankiaddon"
